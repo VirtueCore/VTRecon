ab189a55829a345c3549b96b0ddcc1816375d864
Pioneer876-hack <Orientmanning1@gmail.com>
2026-03-14 13:21:42 -0400

    v0.1.2.1

--- virustotal.py
+++ virustotal.py
@@ -1,7 +1,21 @@
-import os 
+"""
+VTRecon is a Python tool that uses VirusTotal API for scanning
+files, hashes, and URLs from your terminal
+"""
+import os
 import asyncio
 import vt
 
+# ASCII Art
+ART = r"""
+██╗   ██╗████████╗██████╗ ███████╗ ██████╗ ██████╗ ███╗   ██╗
+██║   ██║╚══██╔══╝██╔══██╗██╔════╝██╔════╝██╔═══██╗████╗  ██║
+██║   ██║   ██║   ██████╔╝█████╗  ██║     ██║   ██║██╔██╗ ██║
+╚██╗ ██╔╝   ██║   ██╔══██╗██╔══╝  ██║     ██║   ██║██║╚██╗██║
+ ╚████╔╝    ██║   ██║  ██║███████╗╚██████╗╚██████╔╝██║ ╚████║
+  ╚═══╝     ╚═╝   ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝                                                 
+"""
+print(ART)
 
 # Filename to become created
 FILENAME = 'VirusTotalAPI.txt'
@@ -39,31 +53,46 @@ async def API(file_to_read):
 
     # Using "async with" for the VT Client for unclosed connection
     async with vt.Client(get_api) as CLIENT:
-        # Get user input for VirusTotal
-        VT_HAS = input(str("URL/HASH: "))
-        try:
-            if "https" in VT_HAS:
-                URL_ID = vt.url_id(VT_HAS)
-                URL = await CLIENT.get_object_async("/urls/{}", URL_ID)
-                print(f"\nResults for {VT_HAS}")
-                OBJECTS = URL.last_analysis_stats
-                for key, value in OBJECTS.items():
-                    print(f"\n{key.title()}: {value}")
+        while True:
+            # Get user input for VirusTotal
+            VT_HAS = input(str("\nURL/HASH: "))
+            try:
+                if "https" in VT_HAS:
+                    URL_ID = vt.url_id(VT_HAS)
+                    URL = await CLIENT.get_object_async("/urls/{}", URL_ID)
+                    print(f"\nResults for {VT_HAS}")
+                    OBJECTS = URL.last_analysis_stats
+                    for key, value in OBJECTS.items():
+                        print(f"\n{key.title()}: {value}")
+                else:
+                    # Await async call
+                    FILE_OBJ = await CLIENT.get_object_async(f"/files/{VT_HAS}")
+                    print(f"\nResults for {VT_HAS}:")
+                    OBJECTS = FILE_OBJ.last_analysis_stats
+                    for key, value in OBJECTS.items():
+                        print(f"\n{key.title()} : {value}")
+                
+            except vt.error.APIError as e:
+                # Display error message for VT
+                print("An error has occurred: {e}")
+            # Rescan checker
+            confirm_scan = input("\nScan Again [Y/n]: ").title()
+            if confirm_scan == 'Y':
+                continue
             else:
-                # Await async call
-                FILE_OBJ = await CLIENT.get_object_async(f"/files/{VT_HAS}")
-                print(f"\nResults for {VT_HAS}:")
-                OBJECTS = FILE_OBJ.last_analysis_stats
-                for key, value in OBJECTS.items():
-                    print(f"\n{key.title()} : {value}")
-            
-        except vt.error.APIError as e:
-            # Display error message for VT
-            print("An error has occurred: {e}")
+                print("\nThank you for using VTRecon")
+                break
 
 
 def main():
-    # Ensure FILE_CREATE is NEVER None
+    """
+    Main function for VirusTotal API.
+
+    Ensures that FILE_CREATE is never None. If the file does not exist, it will be created.
+    If the file does exist, it will be used. The function will then run the VirusTotal API
+    using the asyncio module. If the user interrupts the program with Ctrl+C, a message
+    will be printed indicating that the program is exiting.
+    """
     if not os.path.exists(FILENAME):
         FILE_CREATE = API_FILE()
     else:
